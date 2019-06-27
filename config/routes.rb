82eb3093dc12a70909399d990ab6354ce55d2d42
Nicolas Chican <nchican@omacinema.com>
2019-06-27 12:49:31 +0200

    press

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   scope '(:locale)', locale: /fr|es/ do
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   resources :cinemas do
