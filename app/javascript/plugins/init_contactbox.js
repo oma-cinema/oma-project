a66264efccac581d8d7c884528691294554a9222
Nicolas Chican <nchican@omacinema.com>
2019-07-02 22:46:26 +0200

    done

--- app/javascript/plugins/init_contactbox.js
+++ app/javascript/plugins/init_contactbox.js
@@ -6,7 +6,7 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map1',
-    style: 'mapbox://styles/mapbox/streets-v10',
+    style: 'mapbox://styles/omacinema/cjxm8qwxn0jim1cocy9oyxv70',
     center: [2.3455562, 48.8753697], // starting position as [lng, lat]
     zoom: 12
   });
