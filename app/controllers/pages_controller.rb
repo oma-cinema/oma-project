984e3fa96aa1744b2777e2ec8a308bb4609ac22d
Nicolas Chican <nchican@omacinema.com>
2019-07-22 18:46:18 +0200

    content article ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if params[:tag].present?
+      @articles = Article.tagged_with(params[:tag])
+    else
+      @articles = Article.all
+    end
   end
 
   def contact
