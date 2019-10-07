8b847f2ea5200e9efc51fe8011882154198d7a96
Nicolas Chican <nchican@omacinema.com>
2019-10-07 15:51:14 +0200

    done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -139,14 +139,4 @@ $(document).ready(function(){
 
  // Fog for coming soon
 
- function update(e){
-  var x = e.clientX || e.touches[0].clientX
-  var y = e.clientY || e.touches[0].clientY
-
-  document.documentElement.style.setProperty('--cursorX', x + 'px')
-  document.documentElement.style.setProperty('--cursorY', y + 'px')
-}
-
-document.addEventListener('mousemove',update)
-document.addEventListener('touchmove',update)
 
